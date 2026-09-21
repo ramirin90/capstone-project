5db7822f85f140fc591712cd5169af068732fcee
Ramiro Herrera <ramiroherreraruiz@gmail.com>
2026-09-21 15:28:46 -0700

    added first reposity to access database models

---
+++ capstone/src/main/java/com/uop/capstone/repository/CommunityResourceRepository.java
@@ -0,0 +1,9 @@
+package com.uop.capstone.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.uop.capstone.model.CommunityResource;
+
+public interface CommunityResourceRepository extends JpaRepository<CommunityResource, Long>{
+
+}
